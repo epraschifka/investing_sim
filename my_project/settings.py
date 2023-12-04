1264966761bb9bc28d1be7d309f19790a6e670aa
krazyrobot187 <edwardpraschifka@outlook.com>
2023-12-04 21:49:36 +1030

    moved CorsMiddleware up MIDDLEWARE

--- my_project/settings.py
+++ my_project/settings.py
@@ -50,13 +50,13 @@ CRISPY_TEMPLATE_PACK = "bootstrap4"
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "whitenoise.middleware.WhiteNoiseMiddleware",
-    "corsheaders.middleware.CorsMiddleware",
 ]
 
 ROOT_URLCONF = "my_project.urls"
@@ -140,5 +140,4 @@ CSRF_TRUSTED_ORIGINS = ["https://crypto500-cdf2ab56089c.herokuapp.com"]
 
 CORS_ALLOWED_ORIGINS = [
     "https://crypto500-cdf2ab56089c.herokuapp.com",
-    # Add more origins as needed
 ]
