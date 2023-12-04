92adb05fa79b39143fdd599c456da58517f3ce92
Edward Praschifka <109896364+epraschifka@users.noreply.github.com>
2023-12-04 16:54:18 +1030

    Delete backend/venv directory for simplicity

--- backend/venv/my_project/my_app/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class MyappConfig(AppConfig):
-    name = "my_app"
-
-    def ready(self):
-        from priceUpdater import updater
-        updater.start()
--- backend/venv/myproject/priceUpdater/updater.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-from apscheduler.schedulers.background import BackgroundScheduler
-from priceUpdater import getPricesApi
-
-
-def start():
-    scheduler = BackgroundScheduler()
-    scheduler.add_job(getPricesApi.func, "interval", minutes=5)
-    scheduler.start()
