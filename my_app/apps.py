ea7f47de6a6887e465807affd857746125174929
krazyrobot187 <edwardpraschifka@outlook.com>
2023-12-05 14:30:43 +1030

    removed apps/apps.py

--- my_app/apps.py
+++ my_app/apps.py
@@ -1,9 +1,10 @@
 from django.apps import AppConfig
 
 
-class MyappConfig(AppConfig):
+""" class MyappConfig(AppConfig):
     name = "my_app"
 
     def ready(self):
         from priceUpdater import updater
         updater.start()
+ """
